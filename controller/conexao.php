05d4eb19eec81de3d854619f8c2bcdac8be881cf
MarioGuilherme <marioguietec@gmail.com>
2020-07-09 08:33:42 -0300

    Conexão do PHP com MySQL

--- controller/conexao.php
+++ controller/conexao.php
@@ -1,21 +1,23 @@
 <?php
 
-  include 'config.php' ;
+	include 'config.php' ;
 	
 	//1. fazer a conexão com o banco de dados
-  $servename = "localhost";
+	$servename = "localhost";
 	$username = "root";
-  $password = "";
-  $port     = 3307 ;
-	$database = "etechday" ;
+	$password = "usbw";
+	$port     = 3306 ;
+	$database = "aluno" ;
 
 	//2. criar conexão no servidor
-  $conn = mysqli_connect($servename, $username, $password, $database, $port );
-  mysqli_set_charset($conn, "utf8") ;
+	$conn = mysqli_connect($servename, $username, $password, $database, $port );
+	mysqli_set_charset($conn, "utf8") ;
 
 	//2.1 testar conexão
 	if (!$conn){
 		die("falha na conexão: ".mysqli_connect_error());
+	} else{
+		echo ("Deu Certo a conexão com o Banco ;)");
 	}
 
 ?>
---
+++ services/aulaDS-30.06.2020/aluno/listar.php
@@ -0,0 +1,5 @@
+<?php
+
+include "../../../controller/conexao.php";
+
+?>
